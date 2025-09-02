187b58e027794e048b013ac05a7fef84548b76d7
Eduardo Ocampo <eddie@Eduardos-MacBook-Air.local>
2025-09-02 18:07:06 -0500

    Added fitering/search input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 
 import CoinCard from './components/CoinCard';
+import LimitSelector from './components/LimitSelector';
+import FilterInput from './components/FilterInput';
 
 const API_URl = import.meta.env.VITE_API_URL;
 
@@ -11,11 +13,15 @@ function App() {
 
   const [error, setError] = useState(null);
 
+  const [limit, setLimit] = useState(10);
+
+  const [filter, setFilter] = useState('');
+
   useEffect(() => {
     const fetchCoins = async () => {
       try {
         const res = await fetch(
-          `${API_URl}&order=market_cap_desc&per_page=10&page=1&sparkline=false`
+          `${API_URl}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
         );
         if (!res.ok) throw new Error('Failed to fetch data');
         const data = await res.json();
@@ -30,18 +36,31 @@ function App() {
     };
 
     fetchCoins();
-  }, []);
+  }, [limit]);
+
+  const filteredCoins = coins.filter((coin) => {
+    return (
+      coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+      coin.symbol.toLowerCase().includes(filter.toLowerCase())
+    );
+  });
 
   return (
     <div>
       <h1>🚀 Crypto Dash</h1>
       {loading && <p>Loading...</p>}
+      <div className='top-controls'>
+        <FilterInput filter={filter} onFilterChange={setFilter} />
+        <LimitSelector limit={limit} onLimitChange={setLimit} />
+      </div>
       {error && <div className='error'>{error}</div>}
       {!loading && !error && (
         <main className='grid'>
-          {coins.map((coin) => (
-            <CoinCard key={coin.id} coin={coin} />
-          ))}
+          {filteredCoins.length > 0 ? (
+            filteredCoins.map((coin) => <CoinCard key={coin.id} coin={coin} />)
+          ) : (
+            <p>No matching coins.</p>
+          )}
         </main>
       )}
     </div>
